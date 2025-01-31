2895f0464bd0427815e648cc5c45cc934b88f13d
YoungRa19 <randyna19@gmail.com>
2025-01-30 19:41:49 -0600

    solución botones

--- lib/todos/index.dart
+++ lib/todos/index.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:todosapp/data/asignatura.dart';
+import 'package:uuid/uuid.dart';
 
 class Todos extends StatefulWidget {
   const Todos({super.key});
@@ -9,16 +12,105 @@ class Todos extends StatefulWidget {
 }
 
 class _TodosState extends State<Todos> {
-  List<Asignatura> asignatura = [
-    Asignatura(codigo: '4864564564', nombre: "Fisica", curso: 1)
-  ];
+  List<Asignatura> asignaturas = [];
+  TextEditingController _textEditing = TextEditingController();
+  bool _validate = false;
   int number = 0;
 
-  void addSignature(){
-    asignatura.add(Asignatura(codigo: "$number", nombre: "$number", curso: number));
+  @override
+  void dispose() {
+    _textEditing.dispose();
+    super.dispose();
+  }
+
+  void addSignature() {
+    showProcess(context);
+  }
+
+  showProcess(BuildContext context, {Asignatura? asignatura, int? id}) {
+    String title =
+    asignatura == null ? "Nueva Asignatura" : "Editar ${asignatura.nombre}";
+    return showDialog(
+        context: context,
+        builder: (
+            context,
+            ) {
+          return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return AlertDialog(
+                  actionsAlignment: MainAxisAlignment.spaceEvenly,
+                  title: Text(title),
+                  content: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      TextField(
+                        controller: _textEditing,
+                        onChanged: (data) {
+                          setState(() {
+                            _validate = data.isEmpty;
+                          });
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Nombre',
+                            errorText:
+                            _validate ? "No puede ser un valor vacío" : null,
+                            errorStyle: TextStyle(color: Colors.red)),
+                      )
+                    ],
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _validate = false;
+                          });
+                          _textEditing.clear();
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Cancelar",
+                          style: TextStyle(color: Colors.red),
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          if (_textEditing.value.text.isNotEmpty) {
+                            if (asignatura != null) {
+                              asignaturas[id!].nombre = _textEditing.value.text;
+                              this.setState(() {
+                                asignaturas;
+                              });
+                            } else {
+                              asignaturas.add(Asignatura(
+                                  codigo: Uuid().v4(),
+                                  nombre: _textEditing.value.text,
+                                  curso: Random().nextDouble().toInt()));
+                              _textEditing.clear();
+                              this.setState(() {
+                                asignaturas;
+                                _validate = false;
+                              });
+                            }
+                            Navigator.pop(context);
+                          } else {
+                            this.setState(() {
+                              _validate = _textEditing.text.isEmpty;
+                            });
+                          }
+                        },
+                        child: Text(
+                          "Aceptar",
+                          style: TextStyle(color: Colors.blue),
+                        ))
+                  ],
+                );
+              });
+        });
+  }
+
+  void delete(int id) {
+    asignaturas.removeAt(id);
     setState(() {
-      asignatura;
-      number++;
+      asignaturas;
     });
   }
 
@@ -31,43 +123,77 @@ class _TodosState extends State<Todos> {
           style: TextStyle(color: Colors.blue),
         ),
         actions: [
-          IconButton(onPressed: addSignature, icon: Icon(Icons.add), color: Colors.blue,),
+          IconButton(
+            onPressed: addSignature,
+            icon: Icon(Icons.add),
+            color: Colors.blue,
+          ),
         ],
       ),
       body: Container(
-        padding: EdgeInsets.fromLTRB(20, 5, 20, 0),
-        child: ListView.builder(
-          itemCount: asignatura.length,
-          itemBuilder: (context, idx) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  asignatura[idx].nombre,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        //aqui van las funciones de tipo editar
-                      },
-                      icon: Icon(Icons.edit),
-                      color: Colors.orange,
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+        child: Column(
+          children: [
+            if (asignaturas.isEmpty)
+              Expanded(
+                  child: Center(
+                    child: Text(
+                      "No hay datos disponibles",
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
                     ),
-                    IconButton(
-                      onPressed: () {
-                        //aqui va la funcion de tipo borrar
-                      },
-                      icon: Icon(Icons.delete),
-                      color: Colors.red,
-                    ),
-                  ],
+                  )),
+            if (asignaturas.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: asignaturas.length,
+                  itemBuilder: (context, idx) {
+                    return Container(
+                      padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEDE7F6),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              asignaturas[idx].nombre,
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  _textEditing.text = asignaturas[idx].nombre;
+                                  showProcess(context,
+                                      asignatura: asignaturas[idx], id: idx);
+                                },
+                                icon: Icon(Icons.edit),
+                                color: Colors.orange,
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  delete(idx);
+                                },
+                                icon: Icon(Icons.delete),
+                                color: Colors.red,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ],
-            );
-          },
+              ),
+          ],
         ),
       ),
     );
